eb5981c0807bb1b1845fee1b0528061f62825230
Ibtissam Morafik <ibtissammorafik03@gmail.com>
2024-12-03 22:22:55 +0100

    Add files via upload

---
+++ ApplicationChat/Client.java
@@ -0,0 +1,95 @@
+package app;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Client {
+
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String username;
+
+    public Client(Socket socket, String username) {
+        try {
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.username = username;
+        } catch (IOException e) {
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    public void EnvoyerMessage() {
+        try {
+            bufferedWriter.write(username);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+
+            Scanner scanner = new Scanner(System.in);
+            while (socket.isConnected()) {
+                String messageAEnvoyer = scanner.nextLine();
+                bufferedWriter.write(username + ": " + messageAEnvoyer);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+            }
+        } catch (IOException e) {
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    public void EcouterMessage() {
+        new Thread(() -> {
+            while (socket.isConnected()) {
+                try {
+                    String msgDuGroupeChat = bufferedReader.readLine();
+                    if (msgDuGroupeChat != null) {
+                        System.out.println(msgDuGroupeChat); // Affiche le message reçu
+                    }
+                } catch (IOException e) {
+                    closeEverything(socket, bufferedReader, bufferedWriter);
+                    break;
+                }
+            }
+        }).start();
+    }
+
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+        try {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Entrez votre nom pour le groupe chat : ");
+        String username = scanner.nextLine();
+        try {
+            Socket socket = new Socket("localhost", 1234);
+            Client client = new Client(socket, username);
+            client.EcouterMessage();
+            client.EnvoyerMessage();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
